5344ba217a9ebb8110d2668b701d09c42e6d1d2b
Lucas Marciano <lucas.almeida.marciano@gmail.com>
2018-04-28 22:28:45 -0300

    Metodo que muda o formato da listagem
    Inserção de função para capturar quando a tela é girada para não perder o fotmato selecionado.

--- app/src/main/java/com/lucasmarciano/ceep/PostListActivity.kt
+++ app/src/main/java/com/lucasmarciano/ceep/PostListActivity.kt
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.util.Log
 import android.view.*
+
 import com.lucasmarciano.ceep.adapter.PostListAdapter
 import com.lucasmarciano.ceep.model.Post
 import com.lucasmarciano.ceep.retrofit.RetrofitInitializer
@@ -16,12 +17,23 @@ import retrofit2.Response
 
 
 class PostListActivity : AppCompatActivity() {
+    var notes: List<Post>? = null
+    var numColuns: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_list)
 
+        savedInstanceState?.let{
+            numColuns = savedInstanceState.getInt("coluns")
+        }
+
         initializeRetrofit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt("coluns", numColuns)
     }
 
     private fun initializeRetrofit() {
@@ -29,10 +41,10 @@ class PostListActivity : AppCompatActivity() {
         call.enqueue(object: Callback<List<Post>?> {
             override fun onResponse(call: Call<List<Post>?>?, response: Response<List<Post>?>?) {
                 response?.body()?.let {
-                    val notes: List<Post> = it
+                    notes = it
                     progress_bar.visibility = View.GONE
                     float_new_note.visibility = View.VISIBLE
-                    setupRecicleView(notes)
+                    setupRecicleView()
                 }
             }
 
@@ -45,18 +57,37 @@ class PostListActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.main_menu , menu)
+        val item = menu.getItem(0)
+
+        if(numColuns == 1) {
+            item.icon = resources.getDrawable(R.drawable.ic_in_colum)
+        }else {
+            item.icon = resources.getDrawable(R.drawable.ic_in_list)
+        }
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(numColuns == 1) {
+            numColuns = 2
+            item.icon = resources.getDrawable(R.drawable.ic_in_list)
+        }else {
+            numColuns = 1
+            item.icon = resources.getDrawable(R.drawable.ic_in_colum)
+        }
+        setupRecicleView()
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setupRecicleView(notes: List<Post>) {
+    private fun setupRecicleView() {
         post_list_recyclerview.visibility = View.VISIBLE
-        post_list_recyclerview.adapter = PostListAdapter(notes, this)
-        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        post_list_recyclerview.layoutManager = layoutManager
+        notes?.let {
+            post_list_recyclerview.adapter = PostListAdapter(it, this)
+            val layoutManager = StaggeredGridLayoutManager(numColuns, StaggeredGridLayoutManager.VERTICAL)
+            post_list_recyclerview.layoutManager = layoutManager
+        }
+
     }
 
     fun newNote(view: View) {
